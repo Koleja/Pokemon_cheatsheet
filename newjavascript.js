18a99dc10a0fabb92065a4c29969d02914bddd23
Unknown <koleja93@wp.pl>
2017-07-17 20:36:12 +0200

    small updates

--- newjavascript.js
+++ newjavascript.js
@@ -2,8 +2,25 @@ $(document).ready(function(){
     
     var pokemonNAME, pokePHOTOurl, pokeTYPE, i, j;
     var pokeAPI = "http://pokeapi.co/api/v2/pokemon/";
+    
+  
+    //searching pokemon by user search
+    $('#searchBtn').on('click', function(){
         
-    for (i = 1; i<=10; ++i){
+        $('tbody tr').empty();
+       
+        var searchPoke = $('#pokeSearch').val();
+        console.log(searchPoke);
+        pokeAPI = "http://pokeapi.co/api/v2/pokemon/"+searchPoke;
+        console.log(pokeAPI);
+        
+        $.getJSON(pokeAPI, function(data){
+            
+            var searchPokeId = data.id;
+            var maxSearch = searchPokeId+10;
+            console.log(maxSearch);
+            
+            for (i = searchPokeId; i<=maxSearch; ++i){
          
         var pokeAPI = "http://pokeapi.co/api/v2/pokemon/"+i;
         
@@ -31,7 +48,43 @@ $(document).ready(function(){
                 }
             }
         });  
-    }
+      }
+        });
+        
+    });
+    
+     
+      
+        for (i = 1; i<=10; ++i){
+         
+        var pokeAPI = "http://pokeapi.co/api/v2/pokemon/"+i;
+        
+        $.getJSON(pokeAPI, function(data){
+            
+            pokemonNAME = data.name;
+            pokePHOTOurl = data.sprites.front_default;
+            pokeTYPE = data.types[0].type.name;
+            var pokeID = data.id;
+
+            
+            $('.pokemon_name').append('<th id="pokemon-name'+pokeID+'">'+pokemonNAME+'</th>');
+            $('.pokemon_photo').append('<td id="pokemon-photo'+pokeID+'"><img alt="photo'+pokeID+'" src="'+pokePHOTOurl+'"/></td>');  
+            $('.pokemon_type').append('<td id="pokemon-type'+pokeID+'">'+pokeTYPE+'</br></td>');
+
+            
+            for (j = 0; j < data.stats.length; j++){
+                var pokeHP = data.stats[j];
+                        //console.log(pokeHP);
+                if ( pokeHP.stat.name === 'hp'){
+                    //console.log(pokeHP.base_stat+' '+pokeHP.stat.name);
+                    var hp = pokeHP.base_stat;
+                    //$('#pokemon-hp1').html(hp+' HP');
+                    $('.pokemon_hp').append('<td id="pokemon-hp'+pokeID+'">'+hp+' HP</td>');
+                }
+            }
+        });  
+      }
+    
     var marginSTART = parseInt($('table').css('margin-left'), 10);
     var margin = 0;
 
@@ -74,9 +127,9 @@ $(document).ready(function(){
         $('html, body').animate({scrollTop: moveTo.top}, "slow");
                 
         var id = $(this).attr('id');
-        id.split('');
-        var length = id.length;
-        var nr = id[length-1];
+        //id.split('');
+        //var length = id.length;
+        var nr = id.match(/\d+/);
                 
         var hp = $('#pokemon-hp'+nr).html();
         $('#hp').html(hp);
